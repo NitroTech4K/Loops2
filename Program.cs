1706f8fccaecb9b1a035807e5e2dfed175e5ee0b
NitroTech 4K <crtraer@gmail.com>
2022-11-01 09:41:35 -0300

    Constant beeping test

--- Program.cs
+++ Program.cs
@@ -8,8 +8,28 @@ namespace Loops2
 {
     internal class Program
     {
+        static int x;
+        static int y;
+
+        static bool gameOver;
+
+
+
         static void Main(string[] args)
         {
+            gameOver = false;
+
+            while (gameOver == false)
+            {
+                Console.Write(" Bababooey ");
+                Console.Beep(300, 50);
+                
+            }
+
+            while (gameOver == false)
+            {
+            }
+
         }
     }
 }
